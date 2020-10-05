0b2b4f6a6bf3efed730ac483c0fd88ebca88ac0f
Давлат <davlat51@gmail.com>
2020-10-05 22:05:53 +0300

    Student

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -5,7 +5,14 @@ public class Student {
         System.out.println("Tra tra tra");
     }
 
+    public void song(){
+        System.out.println("I believe I can fly");
+    }
     public static void main(String[] args) {
         Student petya = new Student();
+        petya.song();
+        petya.music();
+        petya.music();
+        petya.music();
     }
 }
