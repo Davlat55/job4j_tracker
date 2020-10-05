dc9ce89e8237f1ba282bb808d8fc530f5278f3e6
Давлат <davlat51@gmail.com>
2020-10-05 21:51:59 +0300

    Dog

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Student {
+    public void music(){
+        System.out.println("Tra tra tra");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+    }
+}
