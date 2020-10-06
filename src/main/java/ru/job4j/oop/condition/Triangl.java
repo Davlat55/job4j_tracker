6dd1bbd08fba3de7545ec1d6d5045f5f3a338e9d
Давлат <davlat51@gmail.com>
2020-10-06 23:01:33 +0300

    Площадь треуголника - ООП

---
+++ src/main/java/ru/job4j/oop/condition/Triangl.java
@@ -0,0 +1,39 @@
+package ru.job4j.oop.condition;
+
+public class Triangl {
+    private PointOOP first;
+    private PointOOP second;
+    private PointOOP third;
+
+    public Triangle(PointOOP ap, PointOOP bp, PointOOP cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        double rsl = (a + b + c) / 2;
+        return rsl;
+    }
+
+
+    public boolean exist(double ab, double ac, double bc) {
+         if (((ab + ac) > bc) && (( ac + bc) > ab) && (( ab + bc) > ac)){
+             return true;
+         }
+        return false;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(period(ab,ac,bc) * (period(ab,ac,bc) - ab) * ((period(ab,ac,bc) - ac) * ((period(ab,ac,bc) - bc));
+        }
+        return rsl;
+    }
+
+}
---
+++ src/test/java/ru/job4j/oop/condition/TrianglTest.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop.condition;
+
+import junit.framework.TestCase;
+
+public class TrianglTest extends TestCase {
+
+    public void testArea() {
+
+    }
+}
