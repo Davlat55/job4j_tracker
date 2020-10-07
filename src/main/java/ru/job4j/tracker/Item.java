be79851a569edc2dbe15cb5b8bff385f5198c6db
Давлат <davlat51@gmail.com>
2020-10-08 02:19:18 +0300

    Дата и время по шаблону

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
@@ -19,4 +22,13 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public static void main(String[] args) {
+        LocalDateTime created = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String createdFormat = created.format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + createdFormat);
+
+    }
+
 }
