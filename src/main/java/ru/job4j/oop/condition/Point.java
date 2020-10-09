82b08d22bb97d5b28d2955e17c14d22e9f30797f
Давлат <davlat51@gmail.com>
2020-10-09 08:58:16 +0300

    Расстояни между точками 3D

--- src/main/java/ru/job4j/oop/condition/Point.java
+++ src/main/java/ru/job4j/oop/condition/Point.java
@@ -36,8 +36,7 @@ public class Point {
     }
 
     public double distance3d(Point that) {
-       double  temp =  pow(this.x - that.x, 2) + pow(this.y - that.y, 2);
-        return sqrt(temp + pow(this.z - that.z, 2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 
     public void info() {
