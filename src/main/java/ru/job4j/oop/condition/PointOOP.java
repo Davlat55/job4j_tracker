0a8f9367f1a8bdafdc9d99fe13c13957381feada
Давлат <davlat51@gmail.com>
2020-10-17 13:40:26 +0300

    Расстояние между точками в трехмерном пространстве. [#362837]

--- src/main/java/ru/job4j/oop/condition/PointOOP.java
+++ src/main/java/ru/job4j/oop/condition/PointOOP.java
@@ -11,21 +11,29 @@ public class PointOOP {
 
     private int z;
 
-    public PointOOP(int first, int second, int third) {
+    public PointOOP(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
 
-        int x = new first;
-        int y = new second;
-        int z = new third;
+    public PointOOP(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     public double distance(PointOOP that) {
-        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3D(PointOOP that) {
+        return  sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 
     public static void main(String[] args) {
         PointOOP a = new PointOOP(0, 0, 1);
         PointOOP b = new PointOOP(0, 2, 1);
-        double dist = a.distance(b);
+        double dist = a.distance3D(b);
         System.out.println(dist);
     }
 }
