df19d3fcc8529f593d53ce7cd4abbebb730aed24
Давлат <davlat51@gmail.com>
2020-10-19 23:54:28 +0300

    Дата

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -10,9 +10,10 @@ public class Item {
         return created;
     }
 
-    public class StartUI {
-       Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    public class StartUI {       // создал класс StartUI
+        Item item = new Item();  // создаю объект Item (вот зачем это надо создавать, почему не объясняете?????\
+                                 //  "у него вызываете геттер getCreated()" - это фраза в ступор вводит...
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");  // тут пытаюсь форматировать дату
         private String currentDateTime;
         String currentDateTimeFormat = currentDateTime.format(formatter);
 
@@ -21,6 +22,6 @@ public class Item {
         LocalDateTime currentDateTime = LocalDateTime.now();
         System.out.println("Текущие дата и время до форматирования: " + currentDateTime);
 
-        System.out.println("Текущие дата и время после форматирования: " + currentDateTime);
+        System.out.println("Текущие дата и время после форматирования: " + StartUI.????);   // и здесь печатать как StartUI.created.. но, увы и ах...после обращения через точку он  ничего не видит из класса
     }
 }
