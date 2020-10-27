975a5dba0c13fe10ea95a9fc7d56b749cae74a7a
Давлат <davlat51@gmail.com>
2020-10-27 22:58:29 +0300

    Массив с пустыми ячейками [#362863]

--- src/main/java/ru/job4j/oop/Shop.java
+++ src/main/java/ru/job4j/oop/Shop.java
@@ -2,12 +2,22 @@ package ru.job4j.oop;
 
 public class Shop {
     public static void main(String[] args) {
+        Product products[] = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            }
+        }
 
         Product product = of("Oil", 100);
         System.out.println(product.label());
     }
 
-   public static Product of(String name, int price) {
+    public static Product of(String name, int price) {
 
         if ("Oil".equals(name)) {
             return new LiquidationProduct(name, price).getProduct();
@@ -16,6 +26,14 @@ public class Shop {
     }
 
     public static int indexOfNull(Product[] products) {
-            return -1;
+        int rsl = -1;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product == null) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
     }
 }
