d67ed7a195eeee8b2fa46d22d0f36bf461172916
Давлат <davlat51@gmail.com>
2020-11-08 09:34:33 +0300

    Tracker - [#362868] и Метод замены заявки [#362864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -41,20 +41,20 @@ public class Tracker {
 
 
     public Item[] findAll() {
-        String[] names = {"Petr", null, "Ivan", "Stepan", null};
-        String[] namesWithoutNull = new String[names.length];
-        int size = 0;
-        for (int index = 0; index < names.length; index++) {
-            String name = names[index];
-            if (name != null) {
-                namesWithoutNull[size] = name;
-                size++;
-            }
-        }
-        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
-        for (int index = 0; index < namesWithoutNull.length; index++) {
-            System.out.println(namesWithoutNull[index]);
-        }
+  //      String[] names = {"Petr", null, "Ivan", "Stepan", null};
+  //      String[] namesWithoutNull = new String[names.length];
+  //      int size = 0;
+  //      for (int index = 0; index < names.length; index++) {
+  //          String name = names[index];
+  //          if (name != null) {
+  //              namesWithoutNull[size] = name;
+  //              size++;
+  //          }
+  //      }
+  //      namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
+  //      for (int index = 0; index < namesWithoutNull.length; index++) {
+  //          System.out.println(namesWithoutNull[index]);
+  //      }
         return Arrays.copyOf(items, size);
     }
 
