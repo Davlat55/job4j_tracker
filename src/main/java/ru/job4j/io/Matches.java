87f96dc316719aa49fd2e1032c19de9d84de2dcc
Давлат <davlat51@gmail.com>
2020-11-18 01:09:13 +0300

    Matches

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -5,6 +5,7 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int matches = 11;
+        int vinner = 0;
         System.out.println("На столе 11 спичек, каждый игрок берет от 1 до 3 спичек");
         System.out.println("Кто заберет последные спички, тот победитель");
         while (matches > 0) {
@@ -15,12 +16,12 @@ public class Matches {
                 if (select <= 3) {
                     matches = matches - select;
                     if (matches <= 0) {
-                        System.out.println("Победил игрок № " + i);
-                        break;
+                        vinner = i;
                     }
                 } else
                     System.out.println("Надо брать до 3 спичек. Ход переходит следующему игроку!");
             }
         }
+        System.out.println("Победил игрок № " + vinner);
     }
 }
