e9c5a6811ec41b6736ea7e2ef8c20fd164985f35
Давлат <davlat51@gmail.com>
2020-11-25 00:19:26 +0300

    Реализация класса StartUI [#362800]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class StartUI {
 
      public void init(Scanner scanner, Tracker tracker) {
+
         boolean run = true;
         while (run) {
             this.showMenu();
@@ -14,22 +15,26 @@ public class StartUI {
             if (select == 0) {
                 System.out.println("=== Create a new Item ====");
                 System.out.print("Enter name: ");
-//                String name = scanner.nextLine();
-//                Item item = new Item(name);
-//                tracker.add(item);
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
                 } else if ( select == 1) {
-                System.out.println("Show all items");
-
+               // System.out.println("Show all items");
+                tracker.findAll();
 
                 } else if ( select ==2 ) {
-                System.out.println("Edit item");
+                // System.out.println("Edit item");
+                tracker.replace();
             }
                else if ( select == 3 ) {
-             System.out.println("Delete item");
+             //System.out.println("Delete item");
+                tracker.delete();
          } else if ( select == 4 ) {
-                System.out.println("Find item by Id");
+               // System.out.println("Find item by Id");
+                tracker.findById();
             } else if ( select == 5 ) {
-                System.out.println("Find items by name");
+               // System.out.println("Find items by name");
+                tracker.findByName();
             }
               else if (select == 6) {
                 run = false;
