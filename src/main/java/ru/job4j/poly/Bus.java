ad447a93c9cae90ecbc27703f7c16a701c9b3c9e
Давлат <davlat51@gmail.com>
2020-11-28 17:47:50 +0300

    Интерфейс Input [#362790]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,27 @@
+package ru.job4j.poly;
+
+import java.util.Scanner;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+        System.out.println("We're going!");
+    }
+
+    @Override
+    public void passengers(int passenger) {
+        passenger  =  Integer.valueOf(scanner.nextLine());
+        System.out.println("We have " + passenger + " passengers");
+    }
+
+    @Override
+    public float fill(int gas) {
+        gas = Integer.valueOf(scanner.nextLine());
+
+        float cost = gas * 45;
+
+        System.out.println("We filled up " + gas + "liters for " + cost + " rubles");
+        return 0;
+    }
+}
