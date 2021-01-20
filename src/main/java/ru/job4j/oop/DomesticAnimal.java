80b7daa57b933d7ea8ba7cc9b995fb90f51bcc98
Давлат <davlat51@gmail.com>
2021-01-21 00:59:40 +0300

    Animal

--- src/main/java/ru/job4j/oop/DomesticAnimal.java
+++ src/main/java/ru/job4j/oop/DomesticAnimal.java
@@ -1,6 +1,6 @@
 package ru.job4j.oop;
 
-public class DomesticAnimal extends animal {
+public class DomesticAnimal {
     public void liveOnFarm() {
         System.out.println(nameClass + " живет на ферме, приносит пользу.");
     }
