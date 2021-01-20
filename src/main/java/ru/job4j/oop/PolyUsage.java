bddfbd7718fee85830a512f1193ffee71adffdb4
Давлат <davlat51@gmail.com>
2021-01-21 00:36:19 +0300

    Animal

---
+++ src/main/java/ru/job4j/oop/PolyUsage.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+public class PolyUsage {
+    public static void main(String[] args) {
+        animal animal = new animal();
+        animal domesticAnimal = new DomesticAnimal();
+        animal cow = new Cow();
+        animal goose = new Goose();
+        animal pet = new Pet();
+        animal dog = new Dog();
+        animal guineaPig = new GuineaPig();
+
+        animal[] animals = new animal[]{animal, domesticAnimal, cow, goose, pet, dog, guineaPig};
+        for (animal a : animals) {
+            a.sound();
+        }
+    }
+}
