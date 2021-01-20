62d9e2177945dd4714ed33ed7c069b04f5312df5
Давлат <davlat51@gmail.com>
2021-01-21 00:25:25 +0300

    Animal

---
+++ src/main/java/ru/job4j/oop/Dog2.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+
+public class Dog2 extends Pet{
+    public void runAfterCat() {
+        System.out.println(nameClass + " не любит кошек, постоянно за ними гоняется.");
+    }
+}
