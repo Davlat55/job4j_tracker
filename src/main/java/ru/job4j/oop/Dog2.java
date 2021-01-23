ef45cf3b33c2975a32e72675106640c94c6dba3d
Давлат <davlat51@gmail.com>
2021-01-24 00:48:08 +0300

    Animal

--- src/main/java/ru/job4j/oop/Dog2.java
+++ src/main/java/ru/job4j/oop/Dog2.java
@@ -1,8 +1,7 @@
 package ru.job4j.oop;
 
 public class Dog2 extends Pet {
-    @Override
-    public void sound() {
+       public void sound() {
         System.out.println(getClass().getSimpleName() + " произносит звук: Гав-гав.");
     }
 }
