0da6ab683907be04245acbea5aba6f18ef01847e
Давлат <davlat51@gmail.com>
2021-01-25 20:01:43 +0300

    Animal

--- src/main/java/ru/job4j/oop/Cow.java
+++ src/main/java/ru/job4j/oop/Cow.java
@@ -4,8 +4,5 @@ public class Cow extends DomesticAnimal {
     public void giveMilk() {
         System.out.println(nameClass + " дает человеку молоко.");
     }
-    @Override
-    public void sound() {
-        System.out.println(getClass().getSimpleName() + " произносит звук: Му-му.");
-    }
+
 }
